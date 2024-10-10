c9f9ce2b1c89d80c8c8293c70a946c2ba7e721a9
jesusbonillaupb <jesusenriquebonillaespana1@gmail.com>
2024-10-10 07:47:26 -0500

    wachi wachi wa

---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 8f;
+    public float jumpForce = 10f;
+    private Animator animator;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        animator.SetFloat("horizontalSpeed", Mathf.Abs(moveInput));
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    
+
+    
+}
